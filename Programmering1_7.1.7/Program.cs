f94b3e58c94c18919392dc9623fd83522931367a
Monica Björk <m.bjork95@gmail.com>
2018-04-02 18:35:52 +0200

    Add project files.

---
+++ Programmering1_7.1.7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_7._1._7 {
+    class Program {
+        static void Main(string[] args) {
+            char aChar = (char)97;
+            Console.WriteLine("a " + aChar);
+
+            char bChar = (char)3;
+            Console.WriteLine("b " + bChar);
+
+            char cChar = (char)67;
+            Console.WriteLine("c " + cChar);
+
+            char dChar = (char)200;
+            Console.WriteLine("d " + dChar);
+
+            char eChar = (char)132;
+            Console.WriteLine("e " + eChar);
+
+            Console.ReadLine();
+        }
+    }
+}
